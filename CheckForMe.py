236d4206ff0ffcb27139a4a3be615e0274a2611a
binmom <46296790+binmom@users.noreply.github.com>
2019-01-12 09:46:06 +0900

    Update CheckForMe.py

--- CheckForMe.py
+++ CheckForMe.py
@@ -1,1 +1,7 @@
+change
 
+data_load.py : (0.5,) -> (0.5,0.5,0.5)
+model path : absolute path -> relative path
+
+and Check File with Spyder and erase unused library to clean up!
+Final update is Monday

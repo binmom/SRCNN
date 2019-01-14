2a161c5a7b644b04796a1a224cc47bf49bf56895
Binmom <46296790+binmom@users.noreply.github.com>
2019-01-14 09:56:48 +0900

    Delete CheckForMe.py

--- CheckForMe.py
+++
@@ -1,7 +0,0 @@
-change
-
-data_load.py : (0.5,) -> (0.5,0.5,0.5)
-model path : absolute path -> relative path
-
-and Check File with Spyder and erase unused library to clean up!
-Final update is Monday
